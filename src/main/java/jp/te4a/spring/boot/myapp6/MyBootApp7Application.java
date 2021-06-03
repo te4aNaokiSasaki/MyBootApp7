2ffbff8a3650f39252a1569c42e2e75ee3611557
NaokiSasaki <tejc180574@gmail.com>
2021-06-03 14:24:45 +0900

    kadai7

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/MyBootApp7Application.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootApp7Application.class, args);
+	}
+
+}
